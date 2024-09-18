4efff543ebc1347082cf648b4204d0b323f627d9
jyfhmr <80013819+jyfhmr@users.noreply.github.com>
2024-09-18 10:16:26 -0400

    prueba commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("server on port", "http://localhost:3000")
+  console.log("prueba")
   await app.listen(3000);
 }
 bootstrap();
